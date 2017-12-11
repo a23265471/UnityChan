f14c9f5a8f6246c091ef0839fc05745e4ff55f4f
DESKTOP-8PHRUGV\user <a23265471@gmail.com>
2017-12-12 01:18:52 +0800

    12/12

---
+++ New Unity Project/Assets/Script/UnityChan_Move.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChan_Move : MonoBehaviour {
+
+    public joystick Joystick;
+    private Vector3 movement;
+    private Animator anim;
+    private Rigidbody rigi;
+    public float Speed;
+    public float RotateSpeed;
+    public float Jump;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rigi = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        float x = Joystick.Horizontal();
+        float z = Joystick.Vertical();
+       
+
+        move(x, z);
+    }
+
+    private void move(float x,float z)
+    {
+        movement.Set(x, 0, z);
+        movement = movement * Speed * Time.deltaTime;
+        rigi.MovePosition(transform.position + movement);
+       
+    }
+}
---
+++ New Unity Project/Assets/Script/joystick.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class joystick : MonoBehaviour,IDragHandler,IPointerUpHandler,IPointerDownHandler {
+
+    private Image BGImage;
+    private Image Joystick;
+    private Vector3 InputVector;
+
+
+	// Use this for initialization
+	void Start () {
+
+        BGImage = GetComponent<Image>();
+        Joystick = transform.GetChild(0).GetComponent<Image>();
+	}
+	public virtual void OnPointerDown(PointerEventData ped)
+    {
+        OnDrag(ped);
+
+    }
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+        InputVector = Vector3.zero;
+        Joystick.rectTransform.anchoredPosition = InputVector;
+
+    }
+    public virtual void OnDrag(PointerEventData ped)
+    {
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(BGImage.rectTransform,
+                                                                   ped.position,
+                                                                   ped.pressEventCamera,
+                                                                   out pos)) 
+        {
+            pos.x = (pos.x / BGImage.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / BGImage.rectTransform.sizeDelta.y);
+
+            InputVector = new Vector3(pos.x * 2 - 1, 0, pos.y * 2 - 1);
+            InputVector = (InputVector.magnitude > 1.0f) ? InputVector.normalized : InputVector;
+            Debug.Log(InputVector);
+
+            Joystick.rectTransform.anchoredPosition = new Vector3(InputVector.x * (BGImage.rectTransform.sizeDelta.x / 3)
+                                                                , InputVector.z * (BGImage.rectTransform.sizeDelta.y / 3));
+
+        }
+
+    }
+
+    public float Horizontal()
+    {
+        if (InputVector.x != 0)
+            return InputVector.x;
+        else
+            return Input.GetAxis("Horizontal");
+    }
+
+    public float Vertical()
+    {
+        if (InputVector.z != 0)
+            return InputVector.z;
+        else
+            return Input.GetAxis("Vertical");
+    }
+
+    public float Rotation()
+    {
+        float rotation;
+
+        rotation = Mathf.Atan2(InputVector.x ,InputVector.z)*57.3648f;
+        Debug.Log(rotation);
+        return rotation;
+    }
+
+
+}
