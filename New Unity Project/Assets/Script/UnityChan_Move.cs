dd9914cac0cc85942de50c10ff1208bccb2005f3
DESKTOP-8PHRUGV\user <a23265471@gmail.com>
2017-12-13 01:28:17 +0800

    12/13

--- New Unity Project/Assets/Script/UnityChan_Move.cs
+++ New Unity Project/Assets/Script/UnityChan_Move.cs
@@ -8,24 +8,47 @@ public class UnityChan_Move : MonoBehaviour {
     private Vector3 movement;
     private Animator anim;
     private Rigidbody rigi;
+    private CapsuleCollider PlayerCollider;
     public float Speed;
     public float RotateSpeed;
-    public float Jump;
+    public float JumpHeight;
+    public float PlayerGravity;
+    bool IsGrounded=false;
+    float distToGround;
+    int FloorMask;
 
-    private void Awake()
+    private void Start()
     {
         anim = GetComponent<Animator>();
         rigi = GetComponent<Rigidbody>();
+        PlayerCollider = GetComponent<CapsuleCollider>();
+        distToGround = PlayerCollider.bounds.extents.y;
+        FloorMask= LayerMask.GetMask("Floor");
+        Physics.gravity = new Vector3(0,-(PlayerGravity), 0);
     }
-
-    // Update is called once per frame
-    void FixedUpdate () {
+        // Update is called once per frame
+        void FixedUpdate () {
 
         float x = Joystick.Horizontal();
         float z = Joystick.Vertical();
        
+        if(Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f, FloorMask))
+        {
+            IsGrounded = true;
+            Debug.Log("Grounded");
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                rigi.AddForce(0, JumpHeight, 0, ForceMode.Impulse);
+            }
+
+        }
+        else IsGrounded = false;
+
+
 
         move(x, z);
+        
     }
 
     private void move(float x,float z)
@@ -33,6 +56,16 @@ public class UnityChan_Move : MonoBehaviour {
         movement.Set(x, 0, z);
         movement = movement * Speed * Time.deltaTime;
         rigi.MovePosition(transform.position + movement);
-       
+        
+    }
+    public void Jump()
+    {
+        if (IsGrounded)
+        {
+            Debug.Log("Jump");
+
+            rigi.AddForce(0, JumpHeight, 0, ForceMode.Impulse);
+        }
+
     }
 }
