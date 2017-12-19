76cb450f8103c3e99dfe5eed749eb8f7cc109005
DESKTOP-8PHRUGV\user <a23265471@gmail.com>
2017-12-19 23:57:58 +0800

    12/19 模組更改!

--- New Unity Project/Assets/Script/UnityChan_Move.cs
+++ New Unity Project/Assets/Script/UnityChan_Move.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnityChan_Move : MonoBehaviour {
+public class UnityChan_Move : MonoBehaviour
+{
 
     public joystick Joystick;
     private Vector3 movement;
@@ -16,8 +17,9 @@ public class UnityChan_Move : MonoBehaviour {
     public float PlayerGravity;
     private int JumpCount;
     private bool DoubleJump;
-   
-    bool IsGrounded=false;
+    private bool IsJumping;
+
+    bool IsGrounded = false;
     float distToGround;
     int FloorMask;
 
@@ -28,28 +30,33 @@ public class UnityChan_Move : MonoBehaviour {
         PlayerAC = GetComponent<Animator>();
         FloorMask = LayerMask.GetMask("Floor");
         distToGround = PlayerCollider.bounds.extents.y;
-        Physics.gravity = new Vector3(0,-(PlayerGravity), 0);
+        Physics.gravity = new Vector3(0, -(PlayerGravity), 0);
         DoubleJump = false;
-        
+
     }
-        // Update is called once per frame
-        void FixedUpdate () {
+    // Update is called once per frame
+    void FixedUpdate()
+    {
 
         float x = Joystick.Horizontal();
         float z = Joystick.Vertical();
 
 
 
-        if (Physics.Raycast(transform.position, -Vector3.up, distToGround-0.7f , FloorMask))
+        if (Physics.Raycast(transform.position, -Vector3.up, distToGround - 0.7f, FloorMask))
         {
             Debug.Log(distToGround);
             IsGrounded = true;
             JumpCount = 1;
             Debug.Log("Grounded");
 
+
+
+
+
             //-------------------------Animation------------------------
-           
-             
+
+
 
 
             if (Input.GetKeyUp(KeyCode.Space))
@@ -58,33 +65,40 @@ public class UnityChan_Move : MonoBehaviour {
             }
             Turning();
             move(x, z);
-           
+
         }
         else
         {
             IsGrounded = false;
-            
+
 
 
             move(x, z);
         }
-        
+
     }
 
-    private void move(float x,float z)
+    private void move(float x, float z)
     {
-        if (IsGrounded == false)
+        if (!IsGrounded)
         {
-            Speed = JumpSpeed;
-            
+            movement.Set(x, 0, z);
+            movement = movement * JumpSpeed * Time.deltaTime;
+            rigi.MovePosition(transform.position + movement);
+
         }
-       
-        movement.Set(x, 0, z);
-        movement = movement * Speed * Time.deltaTime;
-        rigi.MovePosition(transform.position + movement);
+        else
+        {
+            movement.Set(x, 0, z);
+            movement = movement * Speed * Time.deltaTime;
+            rigi.MovePosition(transform.position + movement);
+
+        }
+
+
 
         //-----------------Animation----------------------
-        Move_Animation();
+
 
 
     }
@@ -92,14 +106,14 @@ public class UnityChan_Move : MonoBehaviour {
     {
         Quaternion newRotation;
         Vector3 LookAtPoint;
-        if (movement!=new Vector3(0, 0, 0))
+        if (movement != new Vector3(0, 0, 0))
         {
             LookAtPoint = movement;
             newRotation = Quaternion.LookRotation(LookAtPoint);
 
             rigi.MoveRotation(newRotation);
         }
-       
+
     }
     public void Jump()
     {
@@ -107,14 +121,15 @@ public class UnityChan_Move : MonoBehaviour {
         {
             Debug.Log("Jump");
             rigi.AddForce(0, JumpHeight, 0, ForceMode.Impulse);
+
             DoubleJump = true;
-            
-            Jump_Animation();
-            
+            IsJumping = true;
+
+
         }
         else if ((!IsGrounded && JumpCount == 1))
         {
-            Jump_Animation();
+
 
             Debug.Log("DoubleJump");
             rigi.useGravity = false;
@@ -122,41 +137,21 @@ public class UnityChan_Move : MonoBehaviour {
             rigi.AddForce(0, DoubleJumpHeight, 0, ForceMode.Impulse);
             Physics.gravity = new Vector3(0, -(PlayerGravity), 0);
             DoubleJump = false;
+            IsJumping = true;
             JumpCount -= 1;
+
         }
-        
+
     }
 
     private void Move_Animation()
     {
-        if (Joystick.InputVector != new Vector3(0, 0, 0))
+        if (IsGrounded)
         {
-           
-            if (!IsGrounded)
-            {
-                PlayerAC.SetBool("Run", false);
-                Jump_Animation();
-            }
-            else
-            {
-                PlayerAC.SetBool("Run", true);
-                
-            }
-        }
-        else
-        {
-            PlayerAC.SetBool("Run", false);
-           
+            PlayerAC.SetBool("Run", true);
 
         }
-    }
-
-    private void Jump_Animation()
-    {
-        PlayerAC.SetBool("Idle", false);
-        PlayerAC.SetTrigger("Jump");
 
     }
-
 
 }
